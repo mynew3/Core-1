c4d13251dd87af3e8a24409eb9d4102ceb301015
zgn <zgn88@mail.ru>
2017-02-13 22:52:14 +0300

    Core/Script: Firelands update script Alysrazor

--- src/server/scripts/Kalimdor/Firelands/firelands.h
+++ src/server/scripts/Kalimdor/Firelands/firelands.h
@@ -45,6 +45,35 @@ enum FLCreatureIds
     NPC_HARBINGER_OF_FLAME          = 53793,
     NPC_MOLTEN_EGG_TRASH            = 53914,
     NPC_SMOULDERING_HATCHLING       = 53794,
+
+    NPC_DULL_PYRESHELL_FOCUS        = 53963,
+    NPC_SHANNOX                     = 53691,
+    NPC_RAGEFACE                    = 53695, 
+    NPC_RIPLIMB                     = 53694,
+    NPC_RHYOLITH                    = 52558,
+    NPC_BETHTILAC                   = 52498,
+    NPC_ALYSRAZOR                   = 52530, 
+    NPC_BALEROC                     = 53494,
+    NPC_STAGHELM                    = 52571,
+    NPC_RAGNAROS                    = 52409,
+};
+
+enum FLGameObjectIds
+{
+    GO_MOLTEN_METEOR = 208966,
+};
+
+enum FLSpells
+{
+    SPELL_TRANSFORM_CHARGED_PYRESHELL_FOCUS = 100497,
+    SPELL_CREATE_PYRESHELL_FRAGMENT         = 100519,
+    SPELL_SMOLDERING_AURA                   = 101093,
+};
+
+enum FLQuestIds
+{
+    QUEST_HEART_OF_FLAME_ALLIANCE = 29307,
+    QUEST_HEART_OF_FLAME_HORDE    = 29308,
 };
 
 class DelayedAttackStartEvent : public BasicEvent
@@ -61,6 +90,19 @@ class DelayedAttackStartEvent : public BasicEvent
     private:
         Creature* _owner;
 };
+
+static void AddSmoulderingAura(Creature* creature)
+{
+    Map::PlayerList const &PlayerList = creature->GetMap()->GetPlayers();
+    if (!PlayerList.isEmpty())
+        for (Map::PlayerList::const_iterator i = PlayerList.begin(); i != PlayerList.end(); ++i)
+            if (Player* pPlayer = i->GetSource())
+                if (pPlayer->GetQuestStatus(QUEST_HEART_OF_FLAME_ALLIANCE) == QUEST_STATUS_INCOMPLETE || pPlayer->GetQuestStatus(QUEST_HEART_OF_FLAME_HORDE) == QUEST_STATUS_INCOMPLETE)
+                {
+                    creature->CastSpell(creature, SPELL_SMOLDERING_AURA, true);
+                    break;
+                }
+}
 
 template<class AI>
 CreatureAI* GetFirelandsAI(Creature* creature)
